060185263e62e6b3b819469ee63f0c13be8a5203
Zsolt Sándor <ezsolt@gmail.com>
2021-09-27 08:18:43 +0200

    domready promise exp

--- readyPromise/readyPromise.src.js
+++ readyPromise/readyPromise.src.js
@@ -4,26 +4,55 @@
  * Returns a Promise that resolves when the DOM is ready
  * 
  */
+
+ ??????
+const deferred = new Promise(function (resolve) {
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+        const deferred = new Promise(function (resolve) {
+            console.log('_________deferred check')
+            resolve('ready');
+        }
+    else {
+        document.addEventListener('DOMContentLoaded', (start) => resolve());
+    }
+});
+
+export default (fn) => deferred.then(fn);
+
+console.log('Module assigned to window.OneReady. Logged below:')
+console.log(deferred)
+
+deferred.then(() => console.log('One ready.'))
+deferred.then(() => console.log('Two ready.'))
+
+
+
+
+
+    ??????
+
+
 const documentReady = () => new Promise(resolve => {
 
-    const { readyState } = document
+        const { readyState } = document
 
-    // resolve early if DOM is already ready
-    // document ready states: loading, interactive, complete
-    if (readyState === 'interactive' || readyState === 'complete') {
+        // resolve early if DOM is already ready
+        // document ready states: loading, interactive, complete
+        if (readyState === 'interactive' || readyState === 'complete') {
 
-        document.removeEventListener("DOMContentLoaded", resolve);
-        return resolve()
+            document.removeEventListener("DOMContentLoaded", resolve);
+            return resolve()
 
-    }
+        }
 
-    // otherwise, resolve when ready
-    document.addEventListener('DOMContentLoaded', resolve)
-})
+        // otherwise, resolve when ready
+        document.addEventListener('DOMContentLoaded', resolve)
+    })
 
 // export the Promise, not the generator
 const domready = documentReady()
 // export default singleton
+
 
 
 
