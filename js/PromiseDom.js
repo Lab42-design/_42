79b92aaeece1b6621a1dd48ecd30334938db87c3
Zsolt Sándor <ezsolt@gmail.com>
2021-09-30 19:45:27 +0200

    cleanup js export

--- js/PromiseDom.js
+++ js/PromiseDom.js
@@ -1,28 +1,26 @@
 /**
-*
-* usage
-*
-* let dom = new PromiseDom()
-*
-* dom.then(() => start('one'))
-* dom.then(() => start('two'))
-* etc...
-* function start(message) {
-*     console.log('___ startup script: ', message)
-* }
-*/
+ *
+ * document.readyStates = loading || interactive || complete
+ *
+ * usage
+ *
+ * let dom = new PromiseDom()
+ *
+ * dom.then(() => start('one'))
+ * dom.then(() => start('two'))
+ * etc...
+ * function start(message) {
+ *     console.log('___ startup script: ', message)
+ * }
+ */
 class PromiseDom {
     constructor() {
         this.deferred = new Promise(function (resolve, reject) {
             try {
-                // resolve early if DOM is already ready
-                // states: loading, interactive, complete
                 if (document.readyState === 'interactive' || document.readyState === 'complete') {
                     return Promise.resolve();
                 }
-                else {
-                    document.addEventListener('DOMContentLoaded', () => resolve(), false);
-                }
+                document.addEventListener('DOMContentLoaded', () => resolve(), false);
             }
             catch (error) {
                 console.error(error);
