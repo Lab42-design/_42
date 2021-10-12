231b00772072a74a3dd9939d4a1e91ad2dc81fe1
Zsolt Sándor <ezsolt@gmail.com>
2021-10-12 19:37:20 +0200

    update starterkit

--- _project_starter/_main.js
+++ _project_starter/_main.js
@@ -1,8 +1,25 @@
+
+
+// check if dom is ready
 const dom = new PromiseDom()
 dom.deferred.then(() => launch('starting'))
 
+
+
+// launch function
 function launch(message) {
+
     // include partials
+    _include_partials()
+
+
+
+}
+
+
+
+// partials
+function _include_partials() {
     const partial = new FetchPartial()
     partial.fetchAll()
 }
