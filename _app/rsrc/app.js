e92cf1e6bd6ceec7736f3371f5a7e0d88ba859f5
Zsolt Sándor <ezsolt@gmail.com>
2021-10-15 14:41:54 +0200

    app

---
+++ _app/rsrc/app.js
@@ -0,0 +1,27 @@
+//
+//
+//
+
+for( let i = 0; i < 1000000000; i++)
+{
+// This synchronous script is going to delay parsing of the DOM,
+// so the DOMContentLoaded event is going to launch later.
+} 
+
+// check if dom is ready
+const dom = new PromiseDom()
+dom.deferred.then(() => launch('starting'))
+
+// launch function
+function launch(message) {
+
+    // include partials
+    _include_partials()
+
+}
+
+// partials
+function _include_partials() {
+    const partial = new FetchPartial()
+    partial.fetchAll()
+}
