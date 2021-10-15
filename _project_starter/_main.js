ad5f45e5037b010a3149e93c6d1404c00f322e18
Zsolt Sándor <ezsolt@gmail.com>
2021-10-15 14:41:08 +0200

    upd

--- _project_starter/_main.js
+++ _project_starter/_main.js
@@ -1,5 +1,6 @@
-
-
+//
+//
+//
 
 for( let i = 0; i < 1000000000; i++)
 {
@@ -7,16 +8,9 @@ for( let i = 0; i < 1000000000; i++)
 // so the DOMContentLoaded event is going to launch later.
 } 
 
-
-
-
-
-
 // check if dom is ready
 const dom = new PromiseDom()
 dom.deferred.then(() => launch('starting'))
-
-
 
 // launch function
 function launch(message) {
@@ -24,11 +18,7 @@ function launch(message) {
     // include partials
     _include_partials()
 
-
-
 }
-
-
 
 // partials
 function _include_partials() {
